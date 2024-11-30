e06b27d02a912b30d80d02a6cce863c696dc7f0f
timheb16 <65227120+timheb16@users.noreply.github.com>
2024-11-30 19:57:34 +0100

    Update Leaflet Map Layer

--- src/components/App.js
+++ src/components/App.js
@@ -33,10 +33,16 @@ export default function App() {
     /** 
      * Object storing the URL and attribution for the map tiles
      */
-    const esriWorldImagery = {
-        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+    const esriWorldPhysical = {
+        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}',
+        attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service',
     };
+/*
+    const stamenWatercolor = {
+        url: 'https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg}',
+        attribution: '&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>',
+    };*/
+
 
     return (
         <MapContainer
@@ -51,8 +57,8 @@ export default function App() {
             maxBounds={[[90, -180], [-90, 180]]}
             maxBoundsViscosity={1.0}>
             <TileLayer
-                attribution={esriWorldImagery.attribution}
-                url={esriWorldImagery.url}
+                attribution={esriWorldPhysical.attribution}
+                url={esriWorldPhysical.url}
                 tileSize={512}
                 zoomOffset={-1} />
             <ZoomControl
