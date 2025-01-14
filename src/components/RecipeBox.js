e0a673e55188ae36f5b27ccbcc2ca7d710cb1aa2
timheb16 <65227120+timheb16@users.noreply.github.com>
2025-01-14 15:38:11 +0100

    update button colors and margins

--- src/components/RecipeBox.js
+++ src/components/RecipeBox.js
@@ -91,49 +91,52 @@ export default function RecipeBox({ children, titleText, servings, timeText, dif
                         Schwierigkeit: {difficultyText}
                     </Card.Text>
                     {clickedLike&&(
-                    <button 
-                        type="button" 
-                        className="btn btn-primary clicked-button"
+                    <Button 
+                        variant='light'
+                        className="clicked-button"
                         onClick={handleLikeClick}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"></path>
                         </svg>
                         Gefällt mir! { metricsArray[1]&&(metricsArray[1].value + 1)}
-                    </button>
+                    </Button>
                     )}
                     {!clickedLike&&(
-                    <button 
+                    <Button
+                        variant='light'
                         type="button" 
-                        className="btn btn-primary unclicked-button"
+                        className="unclicked-button"
                         onClick={handleLikeClick}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
                             <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"></path>
                         </svg>
                         Gefällt mir { metricsArray[1]&&(metricsArray[1].value)}
-                    </button>
+                    </Button>
                     )}
                     {clickedCooked&&(
-                    <button 
+                    <Button
+                        variant='light'
                         type="button" 
-                        className="btn btn-primary clicked-button"
+                        className="clicked-button"
                         onClick={handleCookedClick}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
                         </svg>
                         Gekocht! { metricsArray[0]&&(metricsArray[0].value + 1)}
-                    </button>
+                    </Button>
                     )}
                     {!clickedCooked&&(
-                    <button 
+                    <Button
+                        variant='light'
                         type="button" 
-                        className="btn btn-primary unclicked-button"
+                        className="unclicked-button"
                         onClick={handleCookedClick}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"></path>
                             <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"></path>
                         </svg>
                         Gekocht { metricsArray[0]&&(metricsArray[0].value)}
-                    </button>
+                    </Button>
                     )}
                 </div>
                 <div
