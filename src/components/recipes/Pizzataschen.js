7894ef9df64eb08744c2491b7163b55040afa0b3
Selina Mutzbauer <selinamutzbauer@posteo.de>
2025-01-14 19:43:39 +0100

    create Pizzataschen,js

---
+++ src/components/recipes/Pizzataschen.js
@@ -0,0 +1,32 @@
+import RecipeBox from '../RecipeBox';
+
+
+/**
+ * Component displaying the recipe and pictures for Momos
+ * 
+ * @param {object} props 
+ * @param {GeoJSON.Feature} props.feature The geo-object that was clicked on by the user
+ * @param {React.Dispatch} props.setFeatureFocus Function to update the value indicating whether a geo-object is currently selected or not
+ * @returns {React.JSX.Element}
+ */
+export default function Pizzataschen({ feature, setFeatureFocus }) {
+
+    return (
+        <div>
+            <RecipeBox
+                titleText={feature.recipe.text.title}
+                timeText={feature.recipe.time}
+                servings={feature.recipe.servings}
+                difficultyText = {feature.recipe.difficulty}
+                cultureText = {feature.recipe.text.culture}
+                ingredientsArray = {feature.recipe.ingredients}
+                preparationTextArray = {feature.recipe.text.preparation}
+                tipsTextArray = {feature.recipe.tips}
+                setFeatureFocus={setFeatureFocus}
+                images={[]}
+                flagImages = {[]}
+                metricsArray = {feature.recipe.metrics}>
+            </RecipeBox>
+        </div>
+    );
+}
